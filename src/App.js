c562539691558f14d16f4da06afd3eb20bf0a505
Michael <kllmmc23@gmail.com>
2020-06-06 18:14:46 -0600

    nesting

--- src/App.js
+++ src/App.js
@@ -1,60 +1,14 @@
 import React from "react";
 import "./App.css";
+import img1 from "./img/img1.jpg";
+import SingleComment from "./SingleComment";
 
 const App = () => {
   return (
     <div className="ui comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img
-            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-            alt="profile pic"
-          />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sarah
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 5:00PM</span>
-          </div>
-          <div className="text">It's amazing</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img
-            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-            alt="profile pic"
-          />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sarah
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 5:00PM</span>
-          </div>
-          <div className="text">It's amazing</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img
-            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-            alt="profile pic"
-          />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sarah
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 5:00PM</span>
-          </div>
-          <div className="text">It's amazing</div>
-        </div>
-      </div>
+      <SingleComment />
+      <SingleComment />
+      <SingleComment />
     </div>
   );
 };
